53359dc2c1cb340d048629c55e488bb6f7fcd5b3
openvela-robot <openvela-robot@xiaomi.com>
2021-06-02 20:57:44 +0800

    runtests: add option -u to error on server unexpectedly alive
    Let's try to actually handle the server unexpectedly alive
    case by first making them visible on CI builds as failures.
    
    This is needed to detect issues with killing of the test
    servers completely including nested process chains with
    multiple PIDs per test server (including bash and perl).
    
    On Windows/cygwin platforms this is especially helpful with
    debugging PID mixups due to cygwin using its own PID space.
    
    Reviewed-by: Daniel Stenberg
    Closes #7180

--- docs/cmdline-opts/user.d
+++ docs/cmdline-opts/user.d
@@ -2,6 +2,8 @@ Long: user
 Short: u
 Arg: <user:password>
 Help: Server user and password
+Category: important auth
+Example: -u user:secret $URL
 ---
 Specify the user name and password to use for server authentication. Overrides
 --netrc and --netrc-optional.
