15e27780c4b2c88b3ef76f2583006a007c678764
liujinye <liujinye@xiaomi.com>
2021-09-23 13:17:23 +0800

    curl-openssl: pass argument to sed single-quoted
    ... instead of using an escaped double-quote. This is an attempt to make
    this work better with ksh that otherwise would insist on a double
    escape!
    
    Reported-by: Randall S. Becker
    Fixes #7758
    Closes #7764

--- docs/cmdline-opts/proxy-tlsauthtype.d
+++ docs/cmdline-opts/proxy-tlsauthtype.d
@@ -2,5 +2,7 @@ Long: proxy-tlsauthtype
 Arg: <type>
 Help: TLS authentication type for HTTPS proxy
 Added: 7.52.0
+Category: proxy tls auth
+Example: --proxy-tlsauthtype SRP -x https://proxy $URL
 ---
 Same as --tlsauthtype but used in HTTPS proxy context.
