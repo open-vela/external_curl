0309cfa3cae602087b72ae546250122a80b13bf7
openvela-robot <openvela-robot@xiaomi.com>
2021-11-10 07:32:46 +0800

    THANKS: add contributors from the 7.80.0 cycle

--- docs/cmdline-opts/proxy-service-name.d
+++ docs/cmdline-opts/proxy-service-name.d
@@ -2,5 +2,7 @@ Long: proxy-service-name
 Arg: <name>
 Help: SPNEGO proxy service name
 Added: 7.43.0
+Category: proxy tls
+Example: --proxy-service-name "shrubbery" -x proxy $URL
 ---
 This option allows you to change the service name for proxy negotiation.
--- docs/cmdline-opts/tlsauthtype.d
+++ docs/cmdline-opts/tlsauthtype.d
@@ -2,6 +2,8 @@ Long: tlsauthtype
 Arg: <type>
 Help: TLS authentication type
 Added: 7.21.4
+Category: tls auth
+Example: --tlsauthtype SRP $URL
 ---
 Set TLS authentication type. Currently, the only supported option is "SRP",
 for TLS-SRP (RFC 5054). If --tlsuser and --tlspassword are specified but
