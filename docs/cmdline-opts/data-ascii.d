69c80abe5056dea1bc14c2f3017fe8dc9d60825d
openvela-robot <openvela-robot@xiaomi.com>
2021-11-09 21:25:02 +0800

    lib/nghttp2: send the ng buffer as block mode
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- docs/cmdline-opts/data-ascii.d
+++ docs/cmdline-opts/data-ascii.d
@@ -2,8 +2,5 @@ Long: data-ascii
 Arg: <data>
 Help: HTTP POST ASCII data
 Protocols: HTTP
-Category: http post upload
-Example: --data-ascii @file $URL
-Added: 7.2
 ---
 This is just an alias for --data.
--- docs/cmdline-opts/tftp-no-options.d
+++ docs/cmdline-opts/tftp-no-options.d
@@ -2,8 +2,6 @@ Long: tftp-no-options
 Help: Do not send any TFTP options
 Protocols: TFTP
 Added: 7.48.0
-Category: tftp
-Example: --tftp-no-options tftp://192.168.0.1/
 ---
 Tells curl not to send TFTP options requests.
 
