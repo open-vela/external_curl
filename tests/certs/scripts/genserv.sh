361e7a32fdc57d2b92c77765cc89557fd40edae9
openvela-robot <openvela-robot@xiaomi.com>
2022-09-03 17:37:10 +0800

    Remove NuttX specific change
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- tests/certs/scripts/genserv.sh
+++ tests/certs/scripts/genserv.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
-#***************************************************************************
-#                                  _   _ ____  _
-#  Project                     ___| | | |  _ \| |
-#                             / __| | | | |_) | |
-#                            | (__| |_| |  _ <| |___
-#                             \___|\___/|_| \_\_____|
-#
-# Copyright (C) 2000 - 2022, EdelWeb for EdelKey and OpenEvidence
-#
-# This software is licensed as described in the file COPYING, which
-# you should have received as part of this distribution. The terms
-# are also available at https://curl.se/docs/copyright.html.
-#
-# You may opt to use, copy, modify, merge, publish, distribute and/or sell
-# copies of the Software, and permit persons to whom the Software is
-# furnished to do so, under the terms of the COPYING file.
-#
-# This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
-# KIND, either express or implied.
-#
-# SPDX-License-Identifier: curl
-#
-###########################################################################
+
+# (c) CopyRight EdelWeb for EdelKey and OpenEvidence, 2000-2004, 2009
+# Author: Peter Sylvester
+
+# "libre" for integration with curl
 
 OPENSSL=openssl
 if [ -f /usr/local/ssl/bin/openssl ] ; then
