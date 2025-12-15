curl/curl

docs: add warnings about FILE: URLs on Windows
 - --url man page section
 - libcurl-security.3 gets the full text
 - CURLOPT_URL.3

Reported-by: Tim Sedlmeyer

docs/cmdline-opts/url.d
@@ -13,3 +13,6 @@ setting a default protocol, see --proto-default for details.
 
 This option may be used any number of times. To control where this URL is
 written, use the --output or the --remote-name options.
+
+Warwning: On Windows, particular file:// accesses can be converted to network
+accesses by the operating system. Beware!

open-vela/external_curl

docs/cmdline-opts/url.d
@@ -13,3 +13,6 @@ setting a default protocol, see --proto-default for details.
 
 This option may be used any number of times. To control where this URL is
 written, use the --output or the --remote-name options.
+
+Warning: On Windows, particular file:// accesses can be converted to network
+accesses by the operating system. Beware!